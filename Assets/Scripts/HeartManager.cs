9bfb99a8d4750c5902860ccf03e171cd3713fb3c
CodeJM <jiaming45@hotmail.com>
2023-08-23 17:38:56 +0800

    Rotating Saw
    Added player at every level

---
+++ Assets/Scripts/HeartManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeartManager : MonoBehaviour
+{
+    [SerializeField] private float startingHealth;
+    public float currentHealth { get; private set; }
+    private Animator animated;
+    private bool dead;
+
+    private void Awake()
+    {
+        currentHealth = startingHealth;
+        animated = GetComponent<Animator>();
+    }
+    public void TakeDamage(float _damage)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+
+        if (currentHealth > 0)
+        {
+            animated.SetTrigger("death");
+            //iframes
+        }
+        else
+        {
+            if (!dead)
+            {
+                animated.SetTrigger("death");
+                GetComponent<PlayerMovement>().enabled = false;
+                dead = true;
+            }
+        }
+    }
+    // public void AddHealth(float _value)
+    // {
+    //     currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
+    // }
+}
--- Assets/Scripts/PlayerLife.cs
+++ Assets/Scripts/PlayerLife.cs
@@ -8,19 +8,36 @@ public class PlayerLife : MonoBehaviour
     private Rigidbody2D rb;
     private Animator animated;
 
+    public int maxLives = 3; // Set the maximum number of lives the player has
+    private int currentLives;
+
     // Start is called before the first frame update
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animated = GetComponent<Animator>();
+        currentLives = maxLives;
     }
 
-    // Update is called once per frame
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Trap"))
         {
+            LoseLife();
+        }
+    }
+
+    private void LoseLife()
+    {
+        currentLives--;
+
+        if (currentLives <= 0)
+        {
             Die();
+        }
+        else
+        {
+            DieAndRestartScene();
         }
     }
 
@@ -28,6 +45,19 @@ public class PlayerLife : MonoBehaviour
     {
         rb.bodyType = RigidbodyType2D.Static;
         animated.SetTrigger("death");
+        // You can also add a game over screen or logic here if needed.
     }
 
+    private void DieAndRestartScene()
+    {
+        rb.bodyType = RigidbodyType2D.Static;
+        animated.SetTrigger("death"); // Show death animation immediately
+        StartCoroutine(RestartLevel());
+    }
+
+    private IEnumerator RestartLevel()
+    {
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
 }
