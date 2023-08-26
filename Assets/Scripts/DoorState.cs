7c60c6e4ce44ad4b6b7a301f8ec5791fae86b440
RuiJing001 <ruijingss@yahoo.com.sg>
2023-08-26 14:07:18 +0800

    going to level 2

--- Assets/Scripts/DoorState.cs
+++ Assets/Scripts/DoorState.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DoorState : MonoBehaviour
 {
@@ -25,6 +26,12 @@ public class DoorState : MonoBehaviour
             isOpen = true;
             // Change the sprite to the open door sprite
             spriteRenderer.sprite = openSprite;
+            Invoke("CompleteLevel", 2f);
         }
+    }
+
+    private void CompleteLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
